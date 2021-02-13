e224cdaf3e08d604aca00c2e2d8c7b44d8f10261
Azamat Omekov <azamatomekov@gmail.com>
2021-02-13 21:25:31 +0600

    change main

--- main.go
+++ main.go
@@ -3,6 +3,16 @@ package testmod
 import "fmt"
 
 // Hi - it is hello method
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s!", name)
+func Hi(name, lang string) string {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name)
+	case "kz":
+		return fmt.Sprintf("Калайсын, %s!", name)
+	case "ru":
+		return fmt.Sprintf("Привет, %s!", name)
+	default:
+		return fmt.Sprintf("#$%^&*(), %s!", name)
+	}
+
 }
